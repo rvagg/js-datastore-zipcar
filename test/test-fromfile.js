588baace3e0838f0bc1857caa3db2b613e0847ee
Rod Vagg <rod@vagg.org>
2019-12-12 19:49:16 +1100

    refactor to create() interfaces with reader/writer backends

---
+++ test/test-fromfile.js
@@ -0,0 +1,42 @@
+/* eslint-env mocha */
+
+const path = require('path')
+const assert = require('assert')
+const { fromFile } = require('../')
+const { acid, makeData, verifyBlocks, verifyHas, verifyRoots } = require('./fixture-data')
+
+describe('From File', () => {
+  before(async () => {
+    return makeData()
+  })
+
+  it('read existing', async () => {
+    const zipDs = await fromFile(path.join(__dirname, 'go.zcar'))
+    await verifyHas(zipDs)
+    await verifyBlocks(zipDs)
+    await verifyRoots(zipDs)
+    await zipDs.close()
+  })
+
+  it('verify only roots', async () => {
+    // tests deferred open for getRoots()
+    const zipDs = await fromFile(path.join(__dirname, 'go.zcar'))
+    await verifyRoots(zipDs)
+    await zipDs.close()
+  })
+
+  // when we instantiate from a file, ZipDatastore should be immutable
+  it('immutable', async () => {
+    const zipDs = await fromFile(path.join(__dirname, 'go.zcar'))
+    await assert.rejects(zipDs.put(acid, Buffer.from('blip')))
+    await assert.rejects(zipDs.delete(acid, Buffer.from('blip')))
+    await assert.rejects(zipDs.setRoots(acid))
+    await assert.rejects(zipDs.setRoots([acid]))
+  })
+
+  it('errors', async () => {
+    const zipDs = await fromFile(path.join(__dirname, 'go.zcar'))
+    await zipDs.close()
+    await assert.rejects(zipDs.close())
+  })
+})
